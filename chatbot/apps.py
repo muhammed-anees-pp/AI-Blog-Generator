f148150d8fedabb485def9ddf575efeab59ce23e
muhammed-anees-pp <muhammedanees.work@gmail.com>
2025-03-01 20:53:25 +0530

    style: added a basic style for chatbot page and added static response

--- chatbot/apps.py
+++ chatbot/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class BlogGeneratorConfig(AppConfig):
+class ChatbotConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'blog_generator'
+    name = 'chatbot'
---
+++ chatbot/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+
+
+# Create your views here.
+def chatbot(request):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = 'Response'
+        return JsonResponse({'message': message, 'response': response})
+    return render(request, 'chatbot.html')
